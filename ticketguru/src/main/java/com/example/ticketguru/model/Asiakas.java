17649243c72e859a3bd003ea87917831d7558fa0
veitim <kenganrakentaja@gmail.com>
2025-09-16 19:23:59 +0300

    asiakas entityä päivitety ja postinumero entity alustettu

--- ticketguru/src/main/java/com/example/ticketguru/model/Asiakas.java
+++ ticketguru/src/main/java/com/example/ticketguru/model/Asiakas.java
@@ -1,8 +1,12 @@
 package com.example.ticketguru.model;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Asiakas {
@@ -11,24 +15,46 @@ public class Asiakas {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long asiakas_id;
     
+    @Column(name = "etunimi", length = 255)
     private String etunimi;
+
+    @Column(name = "sukunimi", length = 255)
     private String sukunimi;
+    
+    @Column(name = "katuosoite", length = 255)
     private String katuosoite;
+
+    @Column(name = "syntymaaika", length = 255)
+    private String syntymaaika;
+
+    @Column(name = "sahkoposti", length = 255)
     private String sahkoposti;
+
+    @Column(name = "puhelinnro", length = 255)
     private String puhelinnro;
+
+    @Column(name = "lisatieto", length = 255)
     private String lisatieto;
+
+    @JsonIgnoreProperties("asiakkaat")
+    @ManyToOne
+    @JoinColumn(name= "postinumero")
+    private Postinumero postinumero;
+
 
     public Asiakas() {
     }
 
     public Asiakas(String etunimi, String sukunimi, String katuosoite, String sahkoposti, String puhelinnro,
-            String lisatieto) {
+            String lisatieto, String syntymaaika, Postinumero postinumero) {
         this.etunimi = etunimi;
         this.sukunimi = sukunimi;
         this.katuosoite = katuosoite;
+        this.syntymaaika = syntymaaika;
         this.sahkoposti = sahkoposti;
         this.puhelinnro = puhelinnro;
         this.lisatieto = lisatieto;
+        this.postinumero = postinumero;
     }
 
     public long getAsiakas_id() {
@@ -87,6 +113,30 @@ public class Asiakas {
         this.lisatieto = lisatieto;
     }
 
-    
+    public String getSyntymaaika() {
+        return syntymaaika;
+    }
+
+    public void setSyntymaaika(String syntymaaika) {
+        this.syntymaaika = syntymaaika;
+    }
+
+    public Postinumero getPostinumero() {
+        return postinumero;
+    }
+
+    public void setPostinumero(Postinumero postinumero) {
+        this.postinumero = postinumero;
+    }
+
+    @Override
+    public String toString() {
+        return "Asiakas [asiakas_id=" + asiakas_id + ", etunimi=" + etunimi + ", sukunimi=" + sukunimi + ", katuosoite="
+                + katuosoite + ", syntymaaika=" + syntymaaika + ", sahkoposti=" + sahkoposti + ", puhelinnro="
+                + puhelinnro + ", lisatieto=" + lisatieto + ", postinumero=" + postinumero +"]";
+    }
+
+
+
     
 }
