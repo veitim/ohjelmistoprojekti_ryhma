d5757ab3828f1b4a002b7ed9cb7bdcedc02d9bee
veitim <kenganrakentaja@gmail.com>
2025-09-23 19:51:02 +0300

    lipputyyppi tehty

---
+++ ticketguru/src/main/java/com/example/ticketguru/model/LippuTyyppi.java
@@ -0,0 +1,78 @@
+package com.example.ticketguru.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class LippuTyyppi {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long tyyppi_id;
+
+    @Column(name = "nimi", length = 150)
+    private String nimi;
+
+    @Column(name = "hinta")
+    private double hinta;
+
+    @JsonIgnoreProperties("tyyppi")
+    @OneToMany(mappedBy= "tyyppi", cascade= CascadeType.ALL, fetch= FetchType.LAZY)
+    private List<Lippu> liput;
+
+    public LippuTyyppi() {
+    }
+
+    public LippuTyyppi(String nimi, double hinta) {
+        this.nimi = nimi;
+        this.hinta = hinta;
+    }
+
+    public long getTyyppi_id() {
+        return tyyppi_id;
+    }
+
+    public void setTyyppi_id(long tyyppi_id) {
+        this.tyyppi_id = tyyppi_id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public double getHinta() {
+        return hinta;
+    }
+
+    public void setHinta(double hinta) {
+        this.hinta = hinta;
+    }
+
+    public List<Lippu> getLiput() {
+        return liput;
+    }
+
+    public void setLiput(List<Lippu> liput) {
+        this.liput = liput;
+    }
+
+    @Override
+    public String toString() {
+        return "LippuTyyppi [tyyppi_id=" + tyyppi_id + ", nimi=" + nimi + ", hinta=" + hinta + "]";
+    }
+
+}
--- ticketguru/src/main/java/com/example/ticketguru/model/LippuTyyppiRepository.java
+++ ticketguru/src/main/java/com/example/ticketguru/model/LippuTyyppiRepository.java
@@ -2,6 +2,6 @@ package com.example.ticketguru.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface KayttajaRepository extends CrudRepository<Kayttaja, Long>{
+public interface LippuTyyppiRepository extends CrudRepository<Tyyppi, Long> {
 
 }
