e85fd9f991484b14278c38fd21b207d86f4fe32e
veitim <kenganrakentaja@gmail.com>
2025-09-23 19:52:08 +0300

    lipputyyppi interface korjattu

--- ticketguru/src/main/java/com/example/ticketguru/model/LippuTyyppiRepository.java
+++ ticketguru/src/main/java/com/example/ticketguru/model/LippuTyyppiRepository.java
@@ -2,6 +2,6 @@ package com.example.ticketguru.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface LippuTyyppiRepository extends CrudRepository<Tyyppi, Long> {
+public interface LippuTyyppiRepository extends CrudRepository<LippuTyyppi, Long> {
 
 }
