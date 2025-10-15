c597d1af2f8cc7e58f51606119d19916103d15ad
Samuel Fizum <samuelfizum@gmail.com>
2025-10-15 17:28:45 +0300

    tehty perus http autentikointi ja riippuvuudet lisätty pomiin

---
+++ ticketguru/src/main/java/com/example/ticketguru/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.ticketguru;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+          .csrf(csrf -> csrf.disable()) 
+          .authorizeHttpRequests(auth -> auth
+              .anyRequest().authenticated()
+          )
+          .httpBasic(Customizer.withDefaults()); 
+        return http.build();
+    }
+
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("samuel")
+            .password("{noop}salasana123") 
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
+
