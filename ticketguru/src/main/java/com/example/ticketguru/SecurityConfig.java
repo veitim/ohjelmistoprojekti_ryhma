e785bb37bacf5c620952c892d6d5f7ef2f3cc93a
Jar1tsu <175611427+jar1tsu@users.noreply.github.com>
2025-11-03 18:28:25 +0200

    Cors v1

--- ticketguru/src/main/java/com/example/ticketguru/SecurityConfig.java
+++ ticketguru/src/main/java/com/example/ticketguru/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.ticketguru;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -28,12 +35,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+          .cors(Customizer.withDefaults())
           .csrf(csrf -> csrf.disable()) 
           .authorizeHttpRequests(auth -> auth
             .anyRequest().authenticated()
           )
           .httpBasic(Customizer.withDefaults()); 
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowedOrigins(List.of("*"));
+      config.setAllowedMethods(List.of("*"));
+      config.setAllowedHeaders(List.of("*"));
+      config.setAllowCredentials(true);
+
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", config);
+      return source;
+
     }
 
     @Autowired
