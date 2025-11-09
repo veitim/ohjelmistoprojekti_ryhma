b91465c68ab6e511a7df69d828ecf7776ea4f8ff
samuel fizum <samuelfizum@gmail.com>
2025-11-09 21:37:42 +0200

    Cors url clientti lisätty

--- ticketguru/src/main/java/com/example/ticketguru/SecurityConfig.java
+++ ticketguru/src/main/java/com/example/ticketguru/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig {
       CorsConfiguration config = new CorsConfiguration();
       
       config.setAllowedOrigins(List.of(
-        "*"
+        "http://localhost:5173",
+        "http://localhost:5174"
       ));
 
       config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
