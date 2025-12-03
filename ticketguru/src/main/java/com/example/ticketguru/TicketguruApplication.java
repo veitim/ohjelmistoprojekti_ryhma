0b5e055ef91367e7fb9ee95b47e0c99aafce5101
veitim <kenganrakentaja@gmail.com>
2025-12-03 14:11:38 +0200

    testi dataan tehty muutoksia

--- ticketguru/src/main/java/com/example/ticketguru/TicketguruApplication.java
+++ ticketguru/src/main/java/com/example/ticketguru/TicketguruApplication.java
@@ -99,21 +99,23 @@ public class TicketguruApplication {
 
 			log.info("Lipputyyppei");
 			LippuTyyppi lipputyyppi1 = new LippuTyyppi("Perus", 50, tapahtuma1);
-			LippuTyyppi lipputyyppi2 = new LippuTyyppi("Perus2", 100, tapahtuma2);
-			LippuTyyppi lipputyyppi3 = new LippuTyyppi("3Päivä", 70, tapahtuma3);
-			LippuTyyppi lipputyyppi4 = new LippuTyyppi("1Päivä", 80, tapahtuma1);
+			LippuTyyppi lipputyyppi2 = new LippuTyyppi("Normi", 100, tapahtuma2);
+			LippuTyyppi lipputyyppi3 = new LippuTyyppi("1Päivä", 70, tapahtuma3);
+			LippuTyyppi lipputyyppi4 = new LippuTyyppi("Vanhus", 80, tapahtuma1);
+			LippuTyyppi lipputyyppi5 = new LippuTyyppi("Ovi", 100, tapahtuma2);
 			ltRepository.save(lipputyyppi1);
 			ltRepository.save(lipputyyppi2);
 			ltRepository.save(lipputyyppi3);
 			ltRepository.save(lipputyyppi4);
+			ltRepository.save(lipputyyppi5);
 
 			Lippu lippu1 = new Lippu("A1", true, lipputyyppi1, "12345a");
-			Lippu lippu2 = new Lippu("manala", false, lipputyyppi2, "12345b");
+			Lippu lippu2 = new Lippu("manala", false, lipputyyppi4, "12345b");
 			Lippu lippu3 = new Lippu("lessinki", false, lipputyyppi3, "12345c");
-			Lippu lippu4 = new Lippu("A2", false, lipputyyppi2, "12345d");
-			Lippu lippu5 = new Lippu("A3", false, lipputyyppi2, "12345e");
-			Lippu lippu6 = new Lippu("A4", false, lipputyyppi2, "12345f");
-			Lippu lippu7 = new Lippu("A5", false, lipputyyppi2, "12345g");
+			Lippu lippu4 = new Lippu("A2", false, lipputyyppi5, "12345d");
+			Lippu lippu5 = new Lippu("A3", false, lipputyyppi5, "12345e");
+			Lippu lippu6 = new Lippu("A4", false, lipputyyppi5, "12345f");
+			Lippu lippu7 = new Lippu("A5", false, lipputyyppi5, "12345g");
 			lippuRepository.save(lippu1);
 			lippuRepository.save(lippu2);
 			lippuRepository.save(lippu3);
@@ -155,20 +157,32 @@ public class TicketguruApplication {
 			log.info("myynti");
 			Myynti myynti1 = new Myynti(kayttaja1, 
 				LocalDate.of(2025, 10, 1),
-				"käteinen", 20.0);
+				"käteinen", 130.0);
 			Myynti myynti2 = new Myynti(kayttaja2, 
-				LocalDate.of(2025, 10, 1), 
-				"kortti", 20.0);
+				LocalDate.of(2025, 10, 2), 
+				"kortti", 70.0);
+			Myynti myynti3 = new Myynti(kayttaja2, 
+				LocalDate.of(2025, 10, 3), 
+				"kortti", 400.0);
 			mRepository.save(myynti1);
 			mRepository.save(myynti2);
+			mRepository.save(myynti3);
 
 			log.info("myyntirivi");
 			Myyntirivi myyntirivi1 = new Myyntirivi(myynti1, lippu1);
-			Myyntirivi myyntirivi2 = new Myyntirivi(myynti1, lippu4);
-			Myyntirivi myyntirivi3 = new Myyntirivi(myynti2, lippu2);
+			Myyntirivi myyntirivi2 = new Myyntirivi(myynti1, lippu2);
+			Myyntirivi myyntirivi3 = new Myyntirivi(myynti2, lippu3);
+			Myyntirivi myyntirivi4 = new Myyntirivi(myynti3, lippu4);
+			Myyntirivi myyntirivi5 = new Myyntirivi(myynti3, lippu5);
+			Myyntirivi myyntirivi6 = new Myyntirivi(myynti3, lippu6);
+			Myyntirivi myyntirivi7 = new Myyntirivi(myynti3, lippu7);
 			mrRepository.save(myyntirivi1);
 			mrRepository.save(myyntirivi2);
 			mrRepository.save(myyntirivi3);
+			mrRepository.save(myyntirivi4);
+			mrRepository.save(myyntirivi5);
+			mrRepository.save(myyntirivi6);
+			mrRepository.save(myyntirivi7);
 
 		};
 	}
